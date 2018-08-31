a3e00a586c8365182aae0b0f07b0469fc2077da7
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-08-31 14:02:16 -0700

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Stylize Expo
+
+This changes some styling for https://expo.io.
+
+The current changes are:
+
+-   Change default font to `'IBM Plex Mono'`.
---
+++ stylizeExpo.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+    'use strict';
+    // Set any vars you want to change here:
+    const codeBox = document.querySelector('.view-lines');
+
+    // Set your values for those vars here:
+    codeBox.style.fontFamily = "'IBM Plex Mono', monospace";
+});
