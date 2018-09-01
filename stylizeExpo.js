e222e1fe002fe3a15e9b4ab6a0de76db3d70aa92
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-09-01 09:06:13 -0700

    Stylize on file changes.

--- stylizeExpo.js
+++ stylizeExpo.js
@@ -1,8 +1,18 @@
-window.addEventListener('load', () => {
-    'use strict';
-    // Set any vars you want to change here:
-    const codeBox = document.querySelector('.view-lines');
+(() => {
+    const stylizeIt = () => {
+        'use strict';
+        // Set any vars you want to change here:
+        const codeBox = document.querySelector('.view-lines');
 
-    // Set your values for those vars here:
-    codeBox.style.fontFamily = "'IBM Plex Mono', monospace";
-});
+        // Set your values for those vars here:
+        codeBox.style.fontFamily = "'IBM Plex Mono', NovaMono, monospace";
+    };
+    window.addEventListener('load', () => {
+        stylizeIt();
+    });
+    document.addEventListener('click', (event) => {
+        if (event.target.tagName === 'DIV') {
+            stylizeIt();
+        }
+    });
+})();
