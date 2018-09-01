698e409de70bcc07ac8e1a2f1abbecb69f3bebb4
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-09-01 12:07:08 -0700

    Switch to building a style element.

--- stylizeExpo.js
+++ stylizeExpo.js
@@ -1,18 +1,15 @@
 (() => {
+    'use strict';
     const stylizeIt = () => {
-        'use strict';
-        // Set any vars you want to change here:
-        const codeBox = document.querySelector('.view-lines');
-
-        // Set your values for those vars here:
-        codeBox.style.fontFamily = "'IBM Plex Mono', NovaMono, monospace";
+        const styleBlock = document.createElement('style');
+        styleBlock.innerHTML = `
+            .view-lines {
+                font-family: 'IBM Plex Mono', NovaMono, monospace !important;
+            }
+        `;
+        document.body.appendChild(styleBlock);
     };
     window.addEventListener('load', () => {
         stylizeIt();
-    });
-    document.addEventListener('click', (event) => {
-        if (event.target.tagName === 'DIV') {
-            stylizeIt();
-        }
     });
 })();
